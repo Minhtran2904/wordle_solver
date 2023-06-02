9ec89091fa0142511446b90929bcfca127a512a9
Minhtran2904 <97359403+Minhtran2904@users.noreply.github.com>
2023-06-02 15:21:52 +0300

    Second commit
    Added worlde_solver() with instruction

--- wordle_solver.py
+++ wordle_solver.py
@@ -2,3 +2,12 @@ def get_word(filename: str):
     with open(filename) as f:
         for line in f:
             guess_list.append(line.strip())
+            
+def wordle_solver():
+    for guesses in range(6):
+        guess = input("\nword: ").lower()
+        print("g - green, y - yellow, w - wrong / grey")
+        feedback = input("Feedback: ").lower()
+        if feedback == "ggggg":
+            print("Well Done! Guess",guesses+1)
+            break
